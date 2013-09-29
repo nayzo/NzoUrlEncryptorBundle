00af81ea21a62b2f9c5e71d82afe8ac973d0165a
nayzo <nayzo@nayzo-comp>
2013-09-29 18:22:01 +0100

    Update

--- UrlEncryptor/UrlEncryptor.php
+++ UrlEncryptor/UrlEncryptor.php
@@ -23,26 +23,33 @@ class UrlEncryptor
         $td = mcrypt_module_open(MCRYPT_DES,"",MCRYPT_MODE_ECB,"");
         $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
         mcrypt_generic_init($td,$key,$iv);
-        $secret = mdecrypt_generic($td, base64_decode($secret));
+        $secret = mdecrypt_generic($td, $this->base64url_decode($secret));
         mcrypt_generic_deinit($td);
 
         if (substr($secret,0,1) != '!')
             return false;
 
         $secret = substr($secret,1,strlen($secret)-1);
-        return unserialize($secret);
+        return $secret;
     }
 
     
     public function encrypt($secret)
     {
         $key = $this->secret; 
-        $secret = serialize($secret);
         $td = mcrypt_module_open(MCRYPT_DES,"",MCRYPT_MODE_ECB,"");
         $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
         mcrypt_generic_init($td,$key,$iv);
-        $secret = base64_encode(mcrypt_generic($td, '!'.$secret));
+        $secret = $this->base64url_encode(mcrypt_generic($td, '!'.$secret));
         mcrypt_generic_deinit($td);
         return $secret;
     }
+    
+    function base64url_encode($data) { 
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+    } 
+
+    function base64url_decode($data) { 
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
+    } 
 }
