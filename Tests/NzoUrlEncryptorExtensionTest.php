a4080244353ebd0b37105e4d19168a72c719144e
Ala Eddine Khefifi <alakhefifi@gmail.com>
2014-10-01 21:25:06 +0100

    Update test

---
+++ Tests/NzoUrlEncryptorExtensionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the NzoUrlEncryptorBundle package.
+ *
+ * (c) Ala Eddine Khefifi <alakhefifi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class NzoUrlEncryptorExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoadDefault()
+    {
+
+
+    }
+
+}
