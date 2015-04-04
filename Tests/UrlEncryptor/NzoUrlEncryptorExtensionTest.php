c13b023ff20232341a8b2832de46a470293b305c
nayzo <alakhefifi@gmail.com>
2015-04-05 00:17:19 +0200

    Enhance unit testing

---
+++ Tests/UrlEncryptor/NzoUrlEncryptorExtensionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the NzoUrlEncryptorBundle package.
+ *
+ * (c) Ala Eddine Khefifi <alakhefifi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nzo\UrlEncryptorBundle\Tests\UrlEncryptor;
+
+use Nzo\UrlEncryptorBundle\UrlEncryptor\UrlEncryptor;
+
+class NzoUrlEncryptorExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    const SECRET = 'secret_text';
+    const KEY = 'encryptionKey';
+    private $urlEncryptor;
+
+    public function setup()
+    {
+        $this->urlEncryptor = new UrlEncryptor(self::KEY);
+    }
+
+    public function testEncrypt()
+    {
+        $encrypted = $this->urlEncryptor->encrypt(self::SECRET);
+        $decrypted = $this->urlEncryptor->decrypt($encrypted);
+
+        $this->assertEquals($decrypted, self::SECRET);
+    }
+
+}
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,11 +9,8 @@
  * file that was distributed with this source code.
  */
 
-class NzoUrlEncryptorExtensionTest extends \PHPUnit_Framework_TestCase
-{
-    public function testLoadDefault()
-    {
-
-    }
-
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install"?');
 }
+
+require $autoloadFile;
