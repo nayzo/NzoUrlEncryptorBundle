9c98132724e645a8ba8c5ae418478c968b7670a8
tgabi333 <tgabi333@gmail.com>
2016-09-23 11:10:31 +0200

    Travis improvements, PSR-4, small changes

--- UrlEncryptor/UrlEncryptor.php
+++ UrlEncryptor/UrlEncryptor.php
@@ -42,12 +42,12 @@ class UrlEncryptor
         return trim($decrypted);
     }
 
-    function base64url_encode($data)
+    protected function base64url_encode($data)
     {
         return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
 
-    function base64url_decode($data)
+    protected function base64url_decode($data)
     {
         return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
     }
