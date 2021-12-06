511df2d5bb1711ed2fc60fd029a6089527e1b9b7
Ala Eddine Khefifi <alakhefifi@gmail.com>
2021-12-06 09:30:04 +0100

    add compatibility for symfony v6

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('nzo_encryptor');
         $rootNode = $treeBuilder->getRootNode();
